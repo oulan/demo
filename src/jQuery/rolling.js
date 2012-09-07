d615f5e5a027e0331fc87d8e3406ce042ceb1fcd
Lanhui Ou <o@iclue.net>
2012-09-07 23:36:24 +0800

    initialize version of Rolling

---
+++ src/jQuery/rolling.js
@@ -0,0 +1,25 @@
+$.fn.Rolling = function(options) {
+    var n = 0;
+    var count = this.children().size();
+
+    var _options = $.extend({}, {time: 1000, fadeOut: 500, fadeIn: 500}, options);
+
+    this.children().hide();
+    this.children().eq(0).show();
+
+    this.children().click(function() {
+
+      $this = $(this);
+      n = $this.index();
+      $this.parent().children().filter(":visible")
+        .fadeOut(_options.fadeOut)
+        .parent().children().eq(n)
+        .fadeIn(_options.fadeIn);
+    });
+
+    this.everyTime(_options.time, this.selector, function() {
+      n = n &gt;= (count -1)? 0 : ++n;
+      $this = $(this);
+      $this.children().eq(n).trigger("click");
+    });
+  };
